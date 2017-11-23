c82ea778896f5d80994312df8dd22b6c44dc12a3
Lucas Pimenta <lucas.pimenta94@outlook.com>
2017-11-23 14:12:57 +0000

    Editando campoDescritor para o padrão 'nome participante - nome do treinamento'

--- forms/TRN-007/events/inputFields.js
+++ forms/TRN-007/events/inputFields.js
@@ -1,5 +1,7 @@
 function inputFields(form){
     var numProcess = getValue("WKNumProces");
+    form.setValue("numProcess", numProcess);
+    form.setValue("campoDescritor", form.getValue("nomeParticipante") + " - " + form.getValue("nomeCurso") );
     if ( activity != 0 ) {
         form.setValue("numProcess", numProcess);
     }
